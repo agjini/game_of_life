db70375c3e233806e35245ffd8c4dc70b512d75f
Augustin Gjini <augustin.gjini@pm.me>
2023-11-03 16:50:25 +0100

    Toggle settings

--- src/main.rs
+++ src/main.rs
@@ -1,27 +1,18 @@
 use bevy::prelude::*;
-use bevy_inspector_egui::quick::WorldInspectorPlugin;
 
 use crate::plugin::GameOfLifePlugin;
 
-mod universe;
 mod cell;
-mod system;
 mod plugin;
+mod system;
 mod timer;
+mod universe;
 
 #[cfg(test)]
 mod test;
 
 fn main() {
     App::new()
-        .add_plugins((
-            DefaultPlugins,
-            GameOfLifePlugin,
-            WorldInspectorPlugin::new().run_if(on_key_press),
-        ))
+        .add_plugins((DefaultPlugins, GameOfLifePlugin))
         .run();
-}
-
-fn on_key_press(input: Res<Input<KeyCode>>) -> bool {
-    input.pressed(KeyCode::F12)
 }
--- src/plugin.rs
+++ src/plugin.rs
@@ -1,23 +1,38 @@
+use crate::cell::Cell;
 use bevy::app::{App, Plugin, Startup, Update};
-use bevy::prelude::{ClearColor, Color};
+use bevy::input::Input;
+use bevy::prelude::{ClearColor, Color, KeyCode, Res, ResMut, Resource};
 use bevy::time::{Timer, TimerMode};
 use bevy::window::close_on_esc;
-use crate::cell::Cell;
+use bevy_inspector_egui::quick::WorldInspectorPlugin;
 
 use crate::system::{click_on_cell, create_universe, entropy, update_cells};
 use crate::timer::StepTimer;
 
 pub struct GameOfLifePlugin;
 
+#[derive(Resource, Eq, PartialEq, Default)]
+struct Settings {
+    display: bool,
+}
+
 impl Plugin for GameOfLifePlugin {
     fn build(&self, app: &mut App) {
-        app
-            .insert_resource(ClearColor(Color::BLACK))
-            .insert_resource(StepTimer(Timer::from_seconds(0.2, TimerMode::Repeating)))
-            .add_systems(Startup, create_universe)
-            .add_systems(Update, update_cells)
-            .add_systems(Update, (click_on_cell, entropy))
-            .add_systems(Update, close_on_esc)
-            .register_type::<Cell>();
+        app.add_plugins(
+            WorldInspectorPlugin::new().run_if(|settings: Res<Settings>| settings.display),
+        )
+        .insert_resource(Settings::default())
+        .insert_resource(ClearColor(Color::BLACK))
+        .insert_resource(StepTimer(Timer::from_seconds(0.2, TimerMode::Repeating)))
+        .add_systems(Startup, create_universe)
+        .add_systems(Update, (update_cells, click_on_cell, entropy))
+        .add_systems(Update, (toggle_settings, close_on_esc))
+        .register_type::<Cell>();
+    }
+}
+
+fn toggle_settings(mut settings: ResMut<Settings>, input: Res<Input<KeyCode>>) {
+    if input.just_pressed(KeyCode::F12) {
+        settings.display = !settings.display;
     }
 }
